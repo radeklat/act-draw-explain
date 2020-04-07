093fa3175d7a43bc5dcd4df290ddf4c51f32a8f4
Radek Lát <radek.lat@elucidata.co.uk>
2020-04-07 20:43:03 +0100

    Initial project set up

---
+++ android/app/src/main/kotlin/sk/lat/act_draw_explain/MainActivity.kt
@@ -0,0 +1,12 @@
+package sk.lat.act_draw_explain
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
