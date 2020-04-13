0d2ab1d6cf24728033985b96f4f5812f737f2ade
Radek Lát <radek.lat@elucidata.co.uk>
2020-04-13 10:23:05 +0100

    Add new topics

--- lib/data/game.dart
+++ lib/data/game.dart
@@ -3,5 +3,5 @@ import 'dart:collection';
 import 'package:act_draw_explain/models/game.dart';
 
 Game game = Game(
-  topicIDs: UnmodifiableListView(List.generate(30, (_index) => 1)),
+  topicIDs: UnmodifiableListView([for(var i=1; i<=3; i+=1) i]),
 );
