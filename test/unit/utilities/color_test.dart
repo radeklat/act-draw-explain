eb3aa42adacd2b684bc77bbae26215008ccc54ee
Radek Lát <radek.lat@elucidata.co.uk>
2020-07-07 20:40:23 +0100

    Add parsing of colors from string names

---
+++ test/unit/utilities/color_test.dart
@@ -0,0 +1,36 @@
+import 'package:act_draw_explain/utilities/color.dart';
+import 'package:flutter/material.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('colorByName should resolve', () {
+    test('colors without shade', () {
+      expect(colorByName("red"), Colors.red.shade500);
+    });
+
+    test('colors with shade', () {
+      expect(colorByName("red500"), Colors.red.shade500);
+      expect(colorByName("red100"), Colors.red.shade100);
+      expect(colorByName("red50"), Colors.red.shade50);
+    });
+
+    test('accented colors without shade', () {
+      expect(colorByName("redAccent"), Colors.redAccent.shade200);
+    });
+
+    test('accented colors with shade', () {
+      expect(colorByName("redAccent200"), Colors.redAccent.shade200);
+      expect(colorByName("redAccent50"), Colors.redAccent.shade50);
+    });
+
+    test('black shades', () {
+      expect(colorByName("black"), Colors.black);
+      expect(colorByName("black12"), Colors.black12);
+    });
+
+    test('white shades', () {
+      expect(colorByName("white"), Colors.white);
+      expect(colorByName("white10"), Colors.white10);
+    });
+  });
+}
